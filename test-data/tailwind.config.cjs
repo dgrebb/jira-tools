d3ce75984df980fd4317b4c6221a1bf46233077a
Dan Grebb <git@dgrebb.com>
2024-03-28 00:57:43 -0400

    feat(svelte): update tailwind config, styles, and favicon

--- test-data/tailwind.config.cjs
+++ test-data/tailwind.config.cjs
@@ -7,7 +7,35 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			'dark-blue': {
+				50: '#e9f1ff',
+				100: '#d6e7ff',
+				200: '#b6d0ff',
+				300: '#8aafff',
+				400: '#5c80ff',
+				500: '#3752ff',
+				600: '#1520ff',
+				700: '#0b13f7',
+				800: '#0c15bc',
+				900: '#141e9b',
+				950: '#0c105a'
+			},
+			cinder: {
+				50: '#eef0ff',
+				100: '#dce0ff',
+				200: '#b2bfff',
+				300: '#6d8aff',
+				400: '#204dff',
+				500: '#0025ff',
+				600: '#0013df',
+				700: '#000eb4',
+				800: '#000d95',
+				900: '#000b7a',
+				950: '#000111'
+			}
+		}
 	},
 
 	plugins: [forms, typography, daisyui]
