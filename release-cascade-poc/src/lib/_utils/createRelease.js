fcc9c183951a5a60ce6bc15f50c2a55027debcfb
Dan Grebb <git@dgrebb.com>
2024-04-03 23:55:35 -0400

    feat(cascade-poc): complete integration of Svelte API route and Jira Server

---
+++ release-cascade-poc/src/lib/_utils/createRelease.js
@@ -0,0 +1,33 @@
+export async function createRelease(name, description, project) {
+	const headers = new Headers();
+	headers.append('content-type', 'application/json');
+	const options = {
+		method: 'POST',
+		headers
+	};
+
+	const body = JSON.stringify({
+		archived: false,
+		description,
+		name,
+		project,
+		released: false
+	});
+
+	try {
+		// Make the API POST call
+		const response = await fetch('/api/v1/create-release', {
+			...options,
+			body
+		});
+
+		if (response.ok) {
+			return 'success'; // Return 'success' if the API call succeeds
+		} else {
+			throw new Error('API call failed');
+		}
+	} catch (error) {
+		console.error('Error:', error);
+		throw error; // Rethrow the error
+	}
+}
