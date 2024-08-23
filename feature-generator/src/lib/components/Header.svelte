4a8d5bbe8d2c1fe69222ab049bc1ee2db4f25e9d
Dan Grebb <git@dgrebb.com>
2024-08-23 08:22:48 -0400

    wip(fg): dashboard styles and interactivity

---
+++ feature-generator/src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Navigation from '@components/Navigation.svelte';
+</script>
+
+<header class=" p-4 sm:px-6 sm:py-0">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Feature Complete</h1>
+	<Navigation />
+</header>
