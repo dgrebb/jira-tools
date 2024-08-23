e2b82132ffe284b727d619f65af0cf4ed818fb41
Dan Grebb <git@dgrebb.com>
2024-08-22 20:20:20 -0400

    build(feature-generator): Feature and related types; sveltekit aliases

---
+++ feature-generator/src/lib/types/features.type.ts
@@ -0,0 +1,41 @@
+export type Task = {
+	issue_type: 'Sub-task' | 'Defect';
+	issue_id: number;
+	parent: number;
+	summary: string;
+	description: string;
+	assignee?: string;
+	reporter?: string;
+	project_name: string;
+	project_key: string;
+	project_type: 'Software';
+};
+
+export type Story = {
+	issue_type: 'Story';
+	issue_id: number;
+	parent: number;
+	summary: string;
+	description: string;
+	assignee?: string;
+	reporter?: string;
+	project_name: string;
+	project_key: string;
+	project_type: 'Software';
+	tasks: Task[];
+};
+
+export type Epic = {
+	issue_type: 'Epic';
+	issue_id: number;
+	summary: string;
+	description: string;
+	project_name: string;
+	project_key: string;
+	project_type: 'Software';
+	stories: Story[];
+};
+
+export type Feature = {
+	epics: Epic[];
+};
