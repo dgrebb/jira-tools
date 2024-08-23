23f8d52f851cf822546be92bea38f0d5402852f0
Dan Grebb <git@dgrebb.com>
2024-08-23 02:33:38 -0400

    build(fg): sveltekit aliases

--- feature-generator/svelte.config.js
+++ feature-generator/svelte.config.js
@@ -1,23 +1,32 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
+
+function createAlias(paths) {
+	const aliases = {};
+
+	Object.entries(paths).forEach(([key, value]) => {
+		const keys = key.split(',').map((k) => k.trim());
+		keys.forEach((alias) => {
+			aliases[alias] = value;
+		});
+	});
+
+	return aliases;
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		alias: {
-			$lib: 'src/lib',
-			'@lib': 'src/lib',
-			'@types': 'src/lib/types',
-			'@mocks': 'src/test/__fixtures__'
-		}
+		alias: createAlias({
+			'$lib, @lib': resolve('src/lib'),
+			'@components, @c': resolve('src/lib/c'),
+			'@mocks': resolve('src/test/__fixtures__'),
+			'@types': resolve('src/lib/t')
+		})
 	}
 };
 
