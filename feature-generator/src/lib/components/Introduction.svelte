6f364aa1ff2acbcbcd47a043289e27271c75f6b8
Dan Grebb <git@dgrebb.com>
2024-12-11 21:55:08 -0500

    feat(fg): switch intro text to user prompt after submission

--- feature-generator/src/lib/components/Introduction.svelte
+++ feature-generator/src/lib/components/Introduction.svelte
@@ -1,5 +1,24 @@
-<p>
-	Welcome! Use this tool to map out your features for Jira. Start by outlining your high-level ideas
-	as Epics—detailing everything down to Sub-tasks is optional. You can always add more details
-	later, so feel free to begin with the big picture and refine it over time. Let's get started!
-</p>
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	interface Props {
+		userMessage: string;
+	}
+
+	let { userMessage }: Props = $props();
+	let messageSet: boolean = $state(false);
+
+	$effect(() => {
+		messageSet = userMessage?.length > 0;
+	});
+</script>
+
+{#if !messageSet}
+	<p class="introduction" transition:fade>
+		Welcome! Use this tool to map out features for Jira. Start by outlining the high-level ideas as
+		Epics—detailing everything down to Sub-tasks is optional. You can always add more details later,
+		so feel free to begin with the big picture and refine it over time. Let's get started!
+	</p>
+{:else}
+	<p class="introduction" transition:fade>{userMessage}</p>
+{/if}
