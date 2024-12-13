8d2dab6936b01273c02ba05b0302e59af51c2e62
Dan Grebb <git@dgrebb.com>
2024-12-13 08:53:54 -0500

    wip(fg): header/nav style adjustments

--- feature-generator/src/lib/components/Header.svelte
+++ feature-generator/src/lib/components/Header.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <header class="flex justify-between p-4 sm:px-6">
-	<h1 class="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl">Feature Complete</h1>
+	<h1 class="scroll-m-20 text-3xl font-extrabold tracking-tight">Feature Complete</h1>
 	<Navigation />
 </header>
